786db02bbabe15122494b56beffe7a9816432384
6im0n <46846093+6im0n@users.noreply.github.com>
2021-04-29 09:36:20 -0400

    Add files via upload

---
+++ downloadPic.py
@@ -0,0 +1,133 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import ui
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+#from bs4 import BeautifulSoup
+#import urllib.request
+import json
+from selenium.common.exceptions import NoSuchElementException
+import re
+#import requests
+import time
+import random
+import os
+import io
+from PIL import Image
+import hashlib
+import shutil 
+import psycopg2
+import string
+from instascrape import *
+from post_to_account import postToAccount
+from config import *
+class thief:
+    def __init__(self):
+        print("stating session....")
+        self.path = path
+        self.driver = webdriver.Firefox()
+        #open database
+        self.db = psycopg2.connect(database=databaseName,user=db_user,password=passwordDataBase,host="127.0.0.1",port="5432")
+        self.cursor = self.db.cursor()
+    def login(self):
+        self.driver.get("https://www.instagram.com/")
+        time.sleep(5)
+        username_input = self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input")
+        username_input.send_keys(username)
+        time.sleep(5)
+        password_input = self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input")
+        password_input.send_keys(password)
+        time.sleep(5)
+        login_button = self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button")
+        login_button.click()
+        time.sleep(5)
+        #ui.WebDriverWait(self.driver, 10).until(self.driver.find_element_by_xpath('//button[text()="Not Now"]')).click()
+        #time.sleep(10)
+        
+    def downloadInstagramPics(self):
+
+        number_photos = int(2)
+        self.driver.get("https://www.instagram.com/explore/")
+        for i in range(1,number_photos):
+
+            #create random Id
+            time.sleep(5)
+            letters = string.ascii_lowercase
+            ID = ''.join(random.choice(letters) for i in range(1,12))
+
+            #click second post
+            post = self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div[1]/div/div[1]/div[3]")
+            ActionChains(self.driver).move_to_element(post).click().perform()
+            time.sleep(5)
+
+            #get current url and current url content
+            current_Url = self.driver.current_url
+            time.sleep(5)
+            print(current_Url)
+            imageScraping = Post(current_Url)
+            headers = {
+                "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.57",
+                "cookie": "sessionid="+self.driver.session_id+";"
+            }
+            print(self.driver.session_id)
+            imageScraping.scrape(headers=headers)
+            print(imageScraping['display_url'])
+
+            #get img url
+            time.sleep(5)
+            complete_url = imageScraping['display_url']
+
+            #check if is video or not
+            video_bool = str(imageScraping['is_video'])
+            print(video_bool)
+            time.sleep(10)
+
+            #download picture
+            if video_bool == "False":
+
+                #get owner of post
+                time.sleep(10)
+                complete_username = str(imageScraping['username'])
+                print("username: "+complete_username)
+
+                #download picture
+                r = requests.get(complete_url)
+                os.chdir(self.path)
+                if r.status_code == 200:
+                    r.raw.decode_content = True
+                    with open("instagramSample.jpeg",'wb') as f:
+                        f.write(r.content)
+                        os.rename("instagramSample.jpeg",ID+".jpeg")
+                        address = self.path + ID + ".jpeg"
+
+                    #store it into a database (postgresql)
+                    ID = "'" + ID + "'"
+                    address = "'"+address+"'"
+                    NOTPOSTED = "'NOTPOSTED'"
+                    complete_username = "'"+complete_username+"'"
+                    print("ID: "+ ID)
+                    print("path: "+address)
+                    print("status: " + NOTPOSTED)
+                    self.cursor.execute("INSERT INTO photos_instagram (id,path,status,owner) \
+      VALUES ({},{},{},{});".format(ID,address,NOTPOSTED,complete_username))
+            else:
+                print("is_video:true")
+                pass
+            
+            #time.sleep(40)
+
+            #go to next post
+            #next_post = self.driver.find_element_by_xpath("/html/body/div[4]div[1]/div/div/a[2]")
+            #next_post.click()                   
+            time.sleep(10)
+
+        self.db.commit()
+        self.db.close()
+
+
+
+bot = thief()
+bot.login()
+bot.downloadInstagramPics()
+postToAccount()
---
+++ post_to_account.py
@@ -0,0 +1,49 @@
+import os
+from instabot import Bot 
+import psycopg2
+import time
+import io
+import string
+from config import *
+
+def postToAccount():
+	print("starting....")
+	db = psycopg2.connect(database=databaseName,user=db_user,password=passwordDataBase,host="127.0.0.1",port="5432")
+	cursor = db.cursor()
+	bot = Bot() 
+	#os.chdir(path)
+	#bot.login(username = username,  
+	#		password = password)
+	print("LOGIN SUCCESS !")
+	r = cursor.execute("SELECT * FROM photos_instagram")
+	rows = cursor.fetchall()
+	for row in rows:
+		owner = row[3]
+		path = row[1]
+		status = row[2]
+		ID = row[0]
+		caption = "Owner: " + "@"+ owner + "\n" + "If you are the owner of the post and want to remove it, please contact me and I will remove it."
+		print(caption);
+		print("")
+		print(status)
+		print("")
+		if str(status) != "POSTED":
+			bot.upload_photo(path,caption=caption)
+			time.sleep(10)
+			#Make change status on DB
+			sql_update_query = """Update photos_instagram set status = %s where id = %s"""
+			cursor.execute(sql_update_query, ("POSTED", ID))
+			connection.commit()
+			count = cursor.rowcount
+			print(count, "Record Updated successfully ")
+			#cursor.execute("SELECT * FROM photos_instagram WHERE id = "+"'"+ID+"'"+";")
+			#cursor.execute("UPDATE photos_instagram SET status = 'POSTED';")
+			os.rename(ID+".REMOVE_ME",ID+".jpeg")
+			print("POSTED")
+		else:
+			pass
+			print("NOTPOSTED")
+
+	db.commit()
+	db.close()
+	print("PostgreSQL connection is closed")
