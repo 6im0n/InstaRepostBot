b10df889995ce6009d6fa130d08c070023116b9a
6im0n <46846093+6im0n@users.noreply.github.com>
2021-04-29 09:34:30 -0400

    Add files via upload

---
+++ db.py
@@ -0,0 +1,38 @@
+import psycopg2
+from config import *
+
+
+def create_tables():
+
+    #""" create tables in the PostgreSQL database"""
+    commands = (
+
+        """
+        CREATE TABLE photos_instagram (
+                id VARCHAR(20) ,
+                path VARCHAR(50),
+                status VARCHAR(15),
+                owner VARCHAR(50)
+        )
+        """)
+    try:
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(
+			database=databaseName, user=db_user, password=passwordDataBase, host='127.0.0.1', port= '5432'
+		)
+        cur = conn.cursor()
+        # create table one by one
+        cur.execute(commands)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == '__main__':
+    create_tables()
