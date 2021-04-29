9681925bb914dc6ffb80f32ea135ded7af047c58
6im0n <46846093+6im0n@users.noreply.github.com>
2021-04-29 09:34:40 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import string
+
+	#DB login
+databaseName ="xxxxxx"
+db_user = "xxxxxxx"
+passwordDataBase = "xxxxxx"
+	#instagram LOGIN
+username = "xxxxxxxx"
+password = "xxxxxxxxx"
+path = "/xxxx/xxxx/xxxxx/"
