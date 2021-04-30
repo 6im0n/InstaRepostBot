6b6117fc91a377a441fdb343bf327eb568aa7f1c
6im0n <46846093+6im0n@users.noreply.github.com>
2021-04-30 21:03:22 +0200

    Update post_to_account.py

--- post_to_account.py
+++ post_to_account.py
@@ -3,6 +3,7 @@ from instabot import Bot
 import psycopg2
 import time
 import io
+import shutil
 import string
 from config import *
 
@@ -11,7 +12,7 @@ def postToAccount():
 	db = psycopg2.connect(database=databaseName,user=db_user,password=passwordDataBase,host="127.0.0.1",port="5432")
 	cursor = db.cursor()
 	#bot = Bot() 
-	#os.chdir(path)
+	os.chdir(Picture_path)
 	#bot.login(username = username,  
 	#		password = password)
 	print("LOGIN SUCCESS !")
@@ -22,7 +23,7 @@ def postToAccount():
 		path = row[1]
 		status = row[2]
 		ID = row[0]
-		caption = "Owner: " + "@"+ owner + "\n" + "————————— \n If you are the owner of the post and want to remove it, please contact me and I will remove it\n—————————\n —————————"
+		caption = "Owner: " + "@"+ owner + "\n" + "————————— \n If you are the owner of the post and want to remove it, please contact me and I will remove it\n—————————\n TAG: \n—————————"
 		print(caption);
 		print("")
 		print(status)
@@ -31,15 +32,15 @@ def postToAccount():
 			#bot.upload_photo(path,caption=caption)
 			time.sleep(0)
 			#Make change status on DB
-			sql_update_query = """Update photos_instagram set status = %s where id = %s"""
-			cursor.execute(sql_update_query% ("'POSTED'", "'"+ID+"'"))
-			db.commit()
+			#sql_update_query = """Update photos_instagram set status = %s where id = %s"""
+			#cursor.execute(sql_update_query% ("'POSTED'", "'"+ID+"'"))
+			#db.commit()
 			count = cursor.rowcount
 			print(count, "Record Updated successfully ")
 			#cursor.execute("SELECT * FROM photos_instagram WHERE id = "+"'"+ID+"'"+";")
-			#cursor.execute("UPDATE photos_instagram SET status = 'POSTED';")
-			#os.rename(ID+".jpeg.REMOVE_ME",ID+".jpeg")
-			os.rmdir("/root/BOT/config")
+			cursor.execute("UPDATE photos_instagram SET status = 'POSTED';")
+			os.rename(ID+".jpeg.REMOVE_ME",ID+".jpeg")
+			shutil.rmtree("HERE THE CONFIG FOLDER CREATE BY INSTABOTAPI")
 			print("POSTED")
 		else:
 			pass
