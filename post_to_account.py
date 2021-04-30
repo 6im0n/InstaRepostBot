09a613287b457fdc843ca0496dbebd39268dd66e
6im0n <46846093+6im0n@users.noreply.github.com>
2021-04-30 20:53:53 +0200

    Update post_to_account.py

--- post_to_account.py
+++ post_to_account.py
@@ -10,10 +10,10 @@ def postToAccount():
 	print("starting....")
 	db = psycopg2.connect(database=databaseName,user=db_user,password=passwordDataBase,host="127.0.0.1",port="5432")
 	cursor = db.cursor()
-	bot = Bot() 
+	#bot = Bot() 
 	#os.chdir(path)
-	bot.login(username = username,  
-			password = password)
+	#bot.login(username = username,  
+	#		password = password)
 	print("LOGIN SUCCESS !")
 	r = cursor.execute("SELECT * FROM photos_instagram")
 	rows = cursor.fetchall()
@@ -22,22 +22,23 @@ def postToAccount():
 		path = row[1]
 		status = row[2]
 		ID = row[0]
-		caption = "Owner: " + "@"+ owner + "\n" + "If you are the owner of the post and want to remove it, please contact me and I will remove it."
+		caption = "Owner: " + "@"+ owner + "\n" + "————————— \n If you are the owner of the post and want to remove it, please contact me and I will remove it\n—————————\n —————————"
 		print(caption);
 		print("")
 		print(status)
 		print("")
 		if str(status) != "POSTED":
-			bot.upload_photo(path,caption=caption)
-			time.sleep(10)
+			#bot.upload_photo(path,caption=caption)
+			time.sleep(0)
 			#Make change status on DB
 			sql_update_query = """Update photos_instagram set status = %s where id = %s"""
-			cursor.execute(sql_update_query, ("POSTED", ID))
+			cursor.execute(sql_update_query% ("'POSTED'", "'"+ID+"'"))
+			db.commit()
 			count = cursor.rowcount
 			print(count, "Record Updated successfully ")
 			#cursor.execute("SELECT * FROM photos_instagram WHERE id = "+"'"+ID+"'"+";")
 			#cursor.execute("UPDATE photos_instagram SET status = 'POSTED';")
-			os.rename(ID+".jpeg.REMOVE_ME",ID+".jpeg")
+			#os.rename(ID+".jpeg.REMOVE_ME",ID+".jpeg")
 			os.rmdir("/root/BOT/config")
 			print("POSTED")
 		else:
@@ -46,4 +47,6 @@ def postToAccount():
 
 	db.commit()
 	db.close()
+	
 	print("PostgreSQL connection is closed")
+postToAccount()
